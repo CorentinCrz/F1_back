9f7170563277fd424948230174b56137f7cea771
CorentinCrz <ccroiz@live.fr>
2019-05-25 17:52:06 +0200

    created entity SummarySeasonConstructor

---
+++ src/Entity/SummarySeasonConstructor.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\SummarySeasonConstructorRepository")
+ */
+class SummarySeasonConstructor
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Constructors")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $constructor;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Drivers")
+     */
+    private $drivers;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $position;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $wins;
+
+    /**
+     * @ORM\Column(type="string", length=25, nullable=true)
+     */
+    private $cumulativeTime;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $fastestLapSpeed;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $mediumGrid;
+
+    public function __construct()
+    {
+        $this->drivers = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getConstructor(): ?Constructors
+    {
+        return $this->constructor;
+    }
+
+    public function setConstructor(?Constructors $constructor): self
+    {
+        $this->constructor = $constructor;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Drivers[]
+     */
+    public function getDrivers(): Collection
+    {
+        return $this->drivers;
+    }
+
+    public function addDriver(Drivers $driver): self
+    {
+        if (!$this->drivers->contains($driver)) {
+            $this->drivers[] = $driver;
+        }
+
+        return $this;
+    }
+
+    public function removeDriver(Drivers $driver): self
+    {
+        if ($this->drivers->contains($driver)) {
+            $this->drivers->removeElement($driver);
+        }
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getWins(): ?int
+    {
+        return $this->wins;
+    }
+
+    public function setWins(int $wins): self
+    {
+        $this->wins = $wins;
+
+        return $this;
+    }
+
+    public function getCumulativeTime(): ?string
+    {
+        return $this->cumulativeTime;
+    }
+
+    public function setCumulativeTime(?string $cumulativeTime): self
+    {
+        $this->cumulativeTime = $cumulativeTime;
+
+        return $this;
+    }
+
+    public function getFastestLapSpeed(): ?float
+    {
+        return $this->fastestLapSpeed;
+    }
+
+    public function setFastestLapSpeed(?float $fastestLapSpeed): self
+    {
+        $this->fastestLapSpeed = $fastestLapSpeed;
+
+        return $this;
+    }
+
+    public function getMediumGrid(): ?int
+    {
+        return $this->mediumGrid;
+    }
+
+    public function setMediumGrid(?int $mediumGrid): self
+    {
+        $this->mediumGrid = $mediumGrid;
+
+        return $this;
+    }
+}
